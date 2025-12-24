baa2314e77a0c106ba15428660ee2ddc98c8ac40
bhavsarrushikesh11 <141747167+bhavsarrushikesh11@users.noreply.github.com>
2025-12-24 11:23:54 +0000

    basic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 // another syntax import express from "express"
+require('dotenv').config()
 const app = express()
 const port = 4000
 
@@ -15,6 +16,10 @@ app.get('/login', (req, res) =>{
     res.send("<h1>rushikeshBhavsar_2004</h1>")
 })
 
-app.listen(port, ()=>{
+// app.listen(port, ()=>{
+//     console.log(`our app listening on port ${port}`)
+// })
+
+app.listen(process.env.PORT, ()=>{
     console.log(`our app listening on port ${port}`)
 })
