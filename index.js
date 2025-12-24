ddc25f0cadd437d70ec160b8fc10fefdedb367d6
bhavsarrushikesh11 <141747167+bhavsarrushikesh11@users.noreply.github.com>
2025-12-24 10:51:31 +0000

    env file

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello world")
+const express = require("express")
+// another syntax import express from "express"
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) =>{
+    res.send("Hello World")
+})
+
+app.get('/twitter', (req, res) =>{
+    res.send("rushikeshBhavsar_2004")
+})
+
+app.get('/login', (req, res) =>{
+    res.send("<h1>rushikeshBhavsar_2004</h1>")
+})
+
+app.listen(port, ()=>{
+    console.log(`our app listening on port ${port}`)
+})
